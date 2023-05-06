921a21fa1bfa00520398fd6d958e8685233998bd
JuanDeDios07 <atoccsahjuandedios@gmail.com>
2023-05-06 12:18:38 -0500

    mi primer comit 01

---
+++ src/main/java/proyecto/pontificia/elp/mi/registromatricula/api/model/Matricula.java
@@ -0,0 +1,30 @@
+package proyecto.pontificia.elp.mi.registromatricula.api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+public class Matricula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idmatricula")
+    private Integer id;
+
+    private String turno;
+
+    private Date fmatricula;
+
+    private String montopagar;
+
+    @ManyToOne
+    @JoinColumn(name = "idalumno")
+    private Alumno alumno;
+
+    @ManyToOne
+    @JoinColumn(name = "idempleado")
+    private Empleado empleado;
+}
